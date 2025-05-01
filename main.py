a1382befd5413ae1d4df0d22513db61adcb5208f
Rithvik Pathi <rithvikddofficial@gmail.com>
2025-05-01 23:44:39 +0530

    save

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import random
 import os
 import streamlit
+
+class Number:
+    def Generate_Number():
+        Ticket_Number = random.randint(1000, 9999)
+        print(Ticket_Number)
+        
+    
+    Generate_Number()
